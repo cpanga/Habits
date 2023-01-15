dcdb193210cedff8e9d8fc4b10dbbfa091513619
Chris Pangalos <chris.pangalos@metaswitch.com>
2023-01-15 06:28:56 +0000

    Added fragment

--- app/src/main/java/com/example/habits/CreateHabitFragment.kt
+++ app/src/main/java/com/example/habits/CreateHabitFragment.kt
@@ -6,14 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.habits.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class CreateHabitFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
