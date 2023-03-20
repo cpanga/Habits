35a8c942c39b24b7ca46857d5eedeb01e2785c2f
Chris Pangalos <chris.pangalos@metaswitch.com>
2023-03-20 04:18:54 +0000

    Add skeleton UTs

---
+++ app/src/test/java/com/example/habits/MainViewModelTest.kt
@@ -0,0 +1,25 @@
+
+package com.example.habits
+
+import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class ViewModelTests {
+
+    // TODO - doesn't work because need application passed in. Solution is to use dependency injection?
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+    private val application: Application = mock(Application::class.java)
+
+    @Test
+    fun quantity_twelve_cupcakes() {
+        val viewModel = MainActivityViewModel(application)
+        viewModel.fabVisible.postValue(false)
+        assertEquals(false, viewModel.fabVisible.value)
+    }
+}
